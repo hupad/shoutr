b5ded66ea8bc7a2f184a2726b2f049a62914d0f6
Harish Upadhyayula <hupadhyayula@dealersocket.com>
2016-09-21 12:27:09 -0500

    adding flash messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921160859) do
+ActiveRecord::Schema.define(version: 20160921165528) do
+
+  create_table "shouts", force: :cascade do |t|
+    t.string   "shout_image_url", limit: 255
+    t.integer  "point",           limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  create_table "user_shouts", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "shout_id",   limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "user_shouts", ["shout_id"], name: "index_user_shouts_on_shout_id", using: :btree
+  add_index "user_shouts", ["user_id"], name: "index_user_shouts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -31,4 +48,6 @@ ActiveRecord::Schema.define(version: 20160921160859) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "user_shouts", "shouts"
+  add_foreign_key "user_shouts", "users"
 end
