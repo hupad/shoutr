8317afb307b5b12892a20b80d9bc5e277757ac9e
Harish Upadhyayula <hupadhyayula@dealersocket.com>
2016-09-22 18:46:12 -0500

    adding some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,87 +12,114 @@ User.delete_all
 			email: Faker::Internet.email,
 			first_name: Faker::Name.first_name,
 			last_name: Faker::Name.last_name,
-			password: Faker::Internet.password
+			password: welcome
 		}
 	)
 end
 
+BadgeCategory.create({
+	name: "General"
+})
+
+BadgeCategory.create({
+	name: "Engineering"
+})
+
+BadgeCategory.create({
+	name: "Design"
+})
 
 Badge.create({
 		name: "Make the Plane",
-		points: 5	
+		points: 5,
+		badge_category_id: 1
 }}
 
 Badge.create({
 		name: "Serve to Win",
-		points: 5	
+		points: 5,
+		badge_category_id: 2
 }}
 
 Badge.create({
 	name: "Orkin Award",
-	points: 5
+	points: 5,
+	badge_category_id: 3
 })
 
 Badge.create({
 	name: "Picasso Award",
-	points: 5
+	points: 5,
+	badge_category_id: 2
 })
 
 Badge.create({
 	name: "Sherlock Award",
-	points: 5	
+	points: 5,
+	badge_category_id: 3
 })
 Badge.create({
 		name: "Go-Go-Gadget Award",
-		points: 5
+		points: 5,
+		badge_category_id: 1
 })
 
 Badge.create({
 		name: "Baby Byte Award",
-		points: 5
+		points: 5,
+		badge_category_id: 1
 })
 
 Badge.create({
 	name: "Java Junkie",
-	points: 5
+	points: 5,
+	badge_category_id: 2
 })
 
 Badge.create({
 	name: "The Mother of Perl Award",
-	points: 5
+	points: 5,
+	badge_category_id: 2
 })
 
 Badge.create({
 	name: "Pixel Pusher",
-	points: 5
+	points: 5,
+	badge_category_id: 3
 })
 
 Badge.create({
 	name: "da Vinci Award",
-	points: 5
+	points: 5,
+	badge_category_id: 1
 })
 
 Badge.create({
 	name: "Quick turn around",
-	points: 5
+	points: 5,
+	badge_category_id: 2
 })
 
 Badge.create({
 	name: "Accepts Criticism",
-	points: 5
+	points: 5,
+	badge_category_id: 3
 })
 
 Badge.create({
 	name: "The Mother of Perl Award",
-	points: 5
+	points: 5,
+	badge_category_id: 1
 })
 
 Badge.create({
 	name: "Mind Craft",
-	points: 5
+	points: 5,
+	badge_category_id: 2
 })
 
 Badge.create({
 	name: "Zuckerberg Award",
-	points: 5
+	points: 5,
+	badge_category_id: 3
 })
